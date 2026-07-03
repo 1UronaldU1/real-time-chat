f116c8ad786d55742f1bce282ee4c6af0719cecb
Ronald Mera <ronald_M_Q_2015@outlook.es>
2026-07-03 09:53:02 -0500

    se crea la carpeta public para seperar los que son back y front se creado public con dos archivos index.html y app.js

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('src/public'));
+
+io.on('connection', (socket) => {
+
+    console.log('Usuario conectado');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Servidor ejecutándose en puerto 3000');
+});
